03dda25c7e58f2f620b064393c736bd30ffb1999
windaris1 <windarisiko@gmail.com>
2026-04-21 19:30:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
 const ConfiguracionCanales = {
-  "ssc": {
-    url: "https://ssc-1-enc.edgenextcdn.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd",
+  "tsn": {
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/7janu55dwc/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "d84c325f36814f39bbe59080272b10c3", k2: "550727de4c96ef1ecff874905493580f" }]
+    keys: [{ k1: "e51aa21f2a0fef9aabc120dfb655b52f", k2: "a12a987fe725a40b6be95cd84b15f689" }]
   },
-  "ssc1": {
-    url: "https://ssc-1-enc.edgenextcdn.net/out/v1/c696e4819b55414388a1a487e8a45ca1/index.mpd",
+  "tsn2": {
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/3n6slkgh54/out/v1/8977baf175da4b94873194613dd3fe55/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "d84c325f36814f39bbe59080272b10c3", k2: "550727de4c96ef1ecff874905493580f" }]
+    keys: [{ k1: "daa8ea92553fd16cd6bfae2b8c582715", k2: "4042e041b5639b79b69ae07a872eef9b" }]
   },
-  "ssc2": {
-    url: "https://ssc-2-enc.edgenextcdn.net/out/v1/a16db2ec338a445a82d9c541cc9293f9/index.mpd",
+  "tsn3": {
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/lzhmuzmjsl/out/v1/c171eeb214c749f2b351c79df317b21e/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "8bcfc55359e24bd7ad1c5560a96ddd3c", k2: "b5dcf721ab522af92a9d3bf0bd55b596" }]
+    keys: [{ k1: "f5c2b30eac11be1e8cdfc9585f5fe6af", k2: "a8198d17bf9b0da77450fbb919a38b2d" }]
   },
-  "ssc3": {
-    url: "https://ssc-3-enc.edgenextcdn.net/out/v1/42e86125555242aaa2a12056832e7814/index.mpd",
+  "tsn4": {
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/h9urpo3cwb/out/v1/fde190f369484bc6b6117cc16cd82a9f/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "7de5dd08ad8041d586c2f16ccc9490a1", k2: "5e1503f3398b34f5099933fedab847ef" }]
+    keys: [{ k1: "c4088f5f265f9de50cffd80bf89308b7", k2: "2c4d2239d96d532b4ec2050653611003" }]
   },
-  "ssc4": {
-    url: "https://ssc-4-enc.edgenextcdn.net/out/v1/5267ea5772874b0db24559d643eaad93/index.mpd",
+  "tsn5": {
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/clients/dash/enc/jkcljgqbx8/out/v1/63bfa701559340f3a2a2cb1fccd24807/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "5c672f6b85a94638872d0214f7806ed4", k2: "bf8756fbb866ee2d5c701c2289dd8de3" }]
+    keys: [{ k1: "e853333da2c9196ea11374634231bc45", k2: "de2e2dbe818d78dfad07d6cd9a4c63d4" }]
   },
-  "ssc5": {
-    url: "https://ssc-5-enc.edgenextcdn.net/out/v1/99289eac5a7b4319905da595afbd792b/index.mpd",
+  "spotv": {
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_spotvhd/default/index.mpd",
     type: "mpd",
-    keys: [{ k1: "c88b512b17ab4f6cb09eb0ff4a1056ed", k2: "adc08ee1c20a734972a55c9aebbd1888" }]
+    keys: [{ k1: "ec7ee27d83764e4b845c48cca31c8eef", k2: "9c0e4191203fccb0fde34ee29999129e" }]
   },
-  "ssc1x": {
-    url: "https://ssc-extra-1-enc.edgenextcdn.net/out/v1/647c58693f1d46af92bd7e69f17912cb/index.mpd",
+  "spotv2": {
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/dr_spotv2hd/default/index.mpd",
     type: "mpd",
-    keys: [{ k1: "ecbc9e6fe6b145efb6658fb5cf7427f8", k2: "03c17e28911f71221acbc0b11f900401" }]
-  },
-  "ssc2x": {
-    url: "https://ssc-extra-2-enc.edgenextcdn.net/out/v1/8b70de2b70d447ba8a7450ba90926a2d/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "4d89249bd4ca4ebc9e70443265f9507d", k2: "cf074ffd2646c9c2f8513b47fa57bc30" }]
-  },
-  "ssc3x": {
-    url: "https://ssc-extra3-enc.edgenextcdn.net/out/v1/8f1c6c3f05ef4284a64b342891bd85ae/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "98cfd6fd4812497fb24dc75f7545f2ee", k2: "d3006ee69e77b25939728ebf30d3180a" }]
-  },
-  "sscnews": {
-    url: "https://ssc-news-live-enc.edgenextcdn.net/out/v1/ef466f43623c4bbaa3f905b566ec35ea/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "3d04975236a44f62857d181597705ee6", k2: "362133e9cb13189ad4fe095ced216f60" }]
+    keys: [{ k1: "7eea72d6075245a99ee3255603d58853", k2: "6848ef60575579bf4d415db1032153ed" }]
   },
   "tudnusa": {
     url: "https://aka-live1-ott.izzigo.tv/1/out/u/dash/TUDN-HD/default.mpd",
@@ -59,7 +44,7 @@ const ConfiguracionCanales = {
     type: "mpd",
     keys: [{ k1: "11223344556677889900112233445566", k2: "4b80724d0ef86bcb2c21f7999d67739d" }]
   },
-  "nova2": {
+  "Nova2": {
     url: "https://dash2.antik.sk/stream/nvidia_nova_sport2/playlist_cbcs.mpd",
     type: "mpd",
     keys: [{ k1: "11223344556677889900112233445566", k2: "4b80724d0ef86bcb2c21f7999d67739d" }]
@@ -105,7 +90,7 @@ const ConfiguracionCanales = {
     keys: [{ k1: "4dbea4b5713a4aa1ae3a2544cd522fc7", k2: "a8323ea99b2d6200a48e1bc27322d548" }]
   },
   "ziggo4": {
-    url: "https://da-d436236420010b88000103030000000000000006.id.cdn.upcbroadband.com/wp/wp4-anp-g05060506-hzn-nl.t1.prd.dyncdn.dmdsdp.com/live/disk1/NL_000097_019370/go-dash-fhd-avc/NL_000097_019370.mpd?p=web",
+    url: "https://da-d436236420010b88000103030000000006.id.cdn.upcbroadband.com/wp4-anp-g05060506-hzn-nl.t1.prd.dyncdn.dmdsdp.com/live/disk1/NL_000097_019370/go-dash-fhd-avc/NL_000097_019370.mpd?p=web",
     type: "mpd",
     keys: [{ k1: "16bf72dc22743d929c4318e193408373", k2: "eae51a1e3904124963074cbf432c7c8e" }]
   },
@@ -114,45 +99,10 @@ const ConfiguracionCanales = {
     type: "mpd",
     keys: [{ k1: "3fb40d85724942f994d86943f48021db", k2: "a6da8742502c8a2153067f5f2a70fb02" }]
   },
-  "ziggo6": {
+  "ZIGGO6": {
     url: "https://mag03.tvx.prd.tv.odido.nl/wh7f454c46tw1024019879_757686866/PLTV/86/224/3221241521/3221241521.mpd?accountinfo=~~V2.0~URnD_afuosWHfY5OEqRXOwfa01c8ac56cf4511de39c2c4a3cab278~iVxKjbtf2gx_dYFqI-vt5C4Cu3COYDjZaw6C_kO2T2wm30fwo1ctD1gr_e2PrgTh48867c3177f3c34842031623cb2e06c9:UTC,",
     type: "mpd",
     keys: [{ k1: "1a0ffa532aa2498490826e2f6a37f7c9", k2: "a8cec27bc7d47909c5b0d8f473b43e8d" }]
-  },
-  "adsp1": {
-    url: "https://uselector.cdn.intigral-ott.net/ADSP1/ADSP1.isml/manifest.mpd",
-    type: "mpd",
-    keys: [{ k1: "eb5a3a48f3e191a00e3ac1e2d470c491", k2: "2c8b7198563527e524d66628c092ef1f" }]
-  },
-  "adsp2": {
-    url: "https://uselector.cdn.intigral-ott.net/ADSP2/ADSP2.isml/manifest.mpd",
-    type: "mpd",
-    keys: [{ k1: "efca93272b9d34f5cf9d8598f43be5ea", k2: "55e5fe489e9c18f6c979c3126d4bcfed" }]
-  },
-  "dazn1es": {
-    url: "https://otte.live.fly.ww.aiv-cdn.net/dub-nitro/live/dash/enc/bmnelo5c7a/out/v1/3ce2cdc4589f46189322bd3717c77957/cenc.mpd",
-    type: "mpd",
-    keys: [{ k1: "44dd9cd370b08a868ead115fe84ecfde", k2: "bff19ab0a51cf14e848389b152913fd0" }]
-  },
-  "dazn2es": {
-    url: "https://otte.live.fly.ww.aiv-cdn.net/dub-nitro/live/dash/enc/xnk4m9bnxt/out/v1/4ced7b7329a54652b9bb0521ed38bd4d/cenc.mpd",
-    type: "mpd",
-    keys: [{ k1: "0eab5a3f3e3b4ba5d42d40ca30d17571", k2: "f3f061ded9b70e8160590d5802ecda6d" }]
-  },
-  "dazn3es": {
-    url: "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/zy1ee5sshp/out/v1/bdcffa69fa3b4f3bb3569c9c73ee1c01/cenc.mpd",
-    type: "mpd",
-    keys: [{ k1: "bad8efff688c0dbb3711e4a7114c22a3", k2: "6ba800673b20776c0c850130d45e1920" }]
-  },
-  "dazn4es": {
-    url: "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/up7qpwch9b/out/v1/a6d5d1a1287b4893b859c2d6ccf2c65d/cenc.mpd",
-    type: "mpd",
-    keys: [{ k1: "d27104d427e4f87e75b19395a9f8796b", k2: "723593c70e2d4c4862754398e80168f8" }]
-  },
-  "daznf1": {
-    url: "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/cqbcvgkb83/out/v1/4dbe05ecfb1540448d82d68eeebfbb1c/cenc.mpd",
-    type: "mpd",
-    keys: [{ k1: "1061be12d303247426ec25e8369b2647", k2: "bd622b0e610295de3b0bccb850ccaaaa" }]
   },
   "daznlaliga": {
     url: "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/wjgklbtvhh/out/v1/659736a1e24c40e4865a80ffd75e7de7/cenc.mpd",
@@ -163,31 +113,6 @@ const ConfiguracionCanales = {
     url: "https://live.ll.ww.aiv-cdn.net/OTTB/dub-nitro/live/clients/dash/enc/tu06vubgf9/out/v1/7668f47d28e345979c072e361f3d3ce3/cenc.mpd",
     type: "mpd",
     keys: [{ k1: "6d08b265a6639e32da7c880d5491e3d2", k2: "0c92556ca14b95f367993710e7cfbc94" }]
-  },
-  "telemundo": {
-    url: "https://live-oneapp-prd-news.akamaized.net/Content/CMAF_OL1-CTR-4s/Live/channel(WSNS)/master.mpd",
-    type: "mpd",
-    keys: [{ k1: "7d6bb9f86e133e4cb33440b493b6b672", k2: "584ad285dcb9e7d42cf3e93f1cc3fe11" }]
-  },
-  "arena1p": {
-    url: "https://bpkcdn.telekom.me/bpk-tv/arena_premium_1/default/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "ddf7a6d83eb05ab48a757b638244af57", k2: "720eb820e035ebe9f9a84e9f258282a1" }, { k1: "96a46b5f1627549db9632ae1172ea62d", k2: "feb9eb6327915c1547ad17c2461b9f66" }, { k1: "7815e3024bf55d5296ece7d188442baa", k2: "4178f0a0b6eed010845a6342a0831f79" }]
-  },
-  "arena2p": {
-    url: "https://bpkcdn.telekom.me/bpk-tv/arena_premium_2/default/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "50e2b92181a35cd9a603bf398660e5c7", k2: "0779f9325ccbb29b5926793236a1537d" }, { k1: "dc5ff1824cd650569eb2252dce236b67", k2: "55e89387b90d67f0fe7b8f7845c3b9e6" }, { k1: "39a06f8583d25d1dbf62a4cee3170342", k2: "5976017af75ba543275891b960e403ab" }]
-  },
-  "arena1": {
-    url: "https://bpkcdn.telekom.me/bpk-tv/arenasport_1/default/index.mpd",
-    type: "mpd",
-    keys: [{ k1: "41c7579e600258b18dc099d15b3d98a4", k2: "949108e0652065214eccde89a1d51210" }, { k1: "feaac16af596581f95f3cf14d9ffcbe9", k2: "f43096d1accd63771f6b4aaafe15e26d" }, { k1: "69835a5f5b9851b6b1a20a195aa0b845", k2: "fc888aecf3ece044c37b2c8b0e672a69" }]
-  },
-  "espn1nl": {
-    url: "https://live-atv-cdn.izzigo.tv/1/out/u/dash/ESPN-HD/default.mpd",
-    type: "mpd",
-    keys: [{ k1: "bb43112b7b72e9c287da0ecdec8606a7", k2: "29e3d2678107f0ab0b35b6537c734f2a" }]
   },
   "espn2nl": {
     url: "https://live-atv-cdn.izzigo.tv/1/out/u/dash/ESPN-2-HD/default.mpd",
@@ -232,7 +157,7 @@ const ConfiguracionCanales = {
   "bein1aus": {
     url: "https://otte.live.fly.ww.aiv-cdn.net/syd-nitro/live/clients/dash/enc/ghwcl6hv68/out/v1/83536910d8034e9b9895a20fbe1c1687/cenc.mpd",
     type: "mpd",
-    keys: [{ k1: "335dad778109954503dcbb21dc92015f", k2: "24bfd75d436cbf73168a2a2dccd40281" }]
+    keys: [{ k1: "335dad778109954503dcbb21dc92015f", k2: "24bfd75d436cbf73168a2dccd40281" }]
   },
   "bein2aus": {
     url: "https://otte.live.fly.ww.aiv-cdn.net/syd-nitro/live/clients/dash/enc/8m8cd46i1t/out/v1/83985c68e4174e90a58a1f2c024be4c9/cenc.mpd",
