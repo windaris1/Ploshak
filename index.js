e3d3ebb94cb037b8fd5d9ede8c7e5407045348fb
windaris1 <windarisiko@gmail.com>
2026-04-24 13:43:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const ConfiguracionCanales = {
     title: "Live"
   },
   "court2": {
-    url: "https://052d33b4b506ff051775da149c5848eb.v.smtcdns.net/play.cbalive.weibisai.com/live/4196008550058061_AiSD.m3u8?txSecret=2db6eb32265ef79976a3371a0ca2031f&txTime=69E5A4C0",
+    url: "https://052d33b4b506ff051775da149c5848eb.v.smtcdns.net/play.cbalive.weibisai.com/live/4187454360838061_AiSD.m3u8?txSecret=3e64de366c2dc2b7173d688b75949fea&txTime=69EC3C40",
     type: "m3u8",
     title: "Live"
   },
   "court22": {
-    url: "https://live5.msrktz.app/live/75915087.m3u8",
+    url: "https://052d33b4b506ff051775da149c5848eb.v.smtcdns.net/play.cbalive.weibisai.com/live/4187454360838061_AiSD.m3u8?txSecret=3e64de366c2dc2b7173d688b75949fea&txTime=69EC3C40",
     type: "m3u8",
     title: "Live"
   },
